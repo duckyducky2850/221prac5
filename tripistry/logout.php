572e66a03893c28739dcec1555ca86a203576a90
Dian Le Roux <lerouxdian1@gmail.com>
2026-05-20 18:21:11 +0200

    Here I just added the requirement for config/db.php in logout.php since BASE_URL is defined in config/db.php

--- tripistry/logout.php
+++ tripistry/logout.php
@@ -1,5 +1,6 @@
 <?php
 /*Destroy session and redirect*/
+require_once __DIR__ . '/config/db.php'; // added confic/db.php because BASE_URL is used
 require_once __DIR__ . '/includes/auth.php';
 session_destroy();
 header('Location: ' . BASE_URL . '/index.php'); exit;
